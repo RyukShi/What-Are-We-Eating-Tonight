d371ed48f264bafde5c3c7fd3876e403b6871364
RyukShi <aure16300P@gmail.com>
2023-01-09 20:49:01 +0100

    New hamster loader

--- src/App.tsx
+++ src/App.tsx
@@ -42,8 +42,6 @@ function App() {
       onPress={() => fetchOpenAI(recipeInstructions(name), false)} />)
   })
 
-  if (error) return (<h1>A problem occurred when fetching data</h1>)
-
   return (
     <div className="App">
       <h1>What are we eating tonight ?</h1>
@@ -68,14 +66,42 @@ function App() {
         </button>
       </form>
 
-      <div style={{ display: (recipes.length > 0) ? "block" : "none" }}>
-        <p>Here are some recipe suggestions, click on any of them to get detailed instructions.</p>
-        <div className="container">
-          {(recipes.length > 0) ? showRecipes : null}
+      {error && (<h1>A problem occurred when fetching data</h1>)}
+
+      {loading && (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
+            <div className="wheel"></div>
+            <div className="hamster">
+              <div className="hamster__body">
+                <div className="hamster__head">
+                  <div className="hamster__ear"></div>
+                  <div className="hamster__eye"></div>
+                  <div className="hamster__nose"></div>
+                </div>
+                <div className="hamster__limb hamster__limb--fr"></div>
+                <div className="hamster__limb hamster__limb--fl"></div>
+                <div className="hamster__limb hamster__limb--br"></div>
+                <div className="hamster__limb hamster__limb--bl"></div>
+                <div className="hamster__tail"></div>
+              </div>
+            </div>
+            <div className="spoke"></div>
+          </div>
         </div>
-      </div>
+      )}
+
+      {!loading && (
+        <div style={{ display: (recipes.length > 0) ? "block" : "none" }}>
+          <p>Here are some recipe suggestions, click on any of them to get detailed instructions.</p>
+          <div className="container">
+            {(recipes.length > 0) ? showRecipes : null}
+          </div>
+        </div>
+      )}
 
       <div style={{ display: (instructions) ? "block" : "none" }}>
+        <h2>Recipe Instructions</h2>
         {(instructions) ? generateHTML(instructions) : null}
       </div>
     </div>
