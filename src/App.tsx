1a032038b3388457c912ba5c2b6af0db0ff2fd2d
RyukShi <aure16300P@gmail.com>
2023-01-10 20:52:54 +0100

    Code restructuring
    New component created HamsterLoader.tsx
    Updated RecipeCard.tsx to disable all buttons when loading is true.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from 'react'
 import './App.css'
 import RecipeCard from './components/RecipeCard'
+import HamsterLoader from './components/HamsterLoader'
 import { recipeInstructions, recipeSuggestions, toList, generateHTML } from './utils'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
   const fetchOpenAI = async (prompt: string, getRecipes: boolean = true) => {
     setLoading(true)
+    setError(null)
     fetch("https://api.openai.com/v1/completions", {
       method: "POST",
       headers: {
@@ -38,8 +40,8 @@ function App() {
   }
 
   const showRecipes = recipes.map((name: string, i: number) => {
-    return (<RecipeCard recipeName={name} key={i}
-      onPress={() => fetchOpenAI(recipeInstructions(name), false)} />)
+    return (<RecipeCard recipeName={name} key={i} disabled={loading}
+      onClick={() => fetchOpenAI(recipeInstructions(name), false)} />)
   })
 
   return (
@@ -66,39 +68,16 @@ function App() {
         </button>
       </form>
 
-      {error && (<h1>A problem occurred when fetching data</h1>)}
+      {error && (<h3>A problem occurred when fetching data</h3>)}
 
-      {loading && (
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-          <div aria-label="Orange and tan hamster running in a metal wheel" role="img" className="wheel-and-hamster">
-            <div className="wheel"></div>
-            <div className="hamster">
-              <div className="hamster__body">
-                <div className="hamster__head">
-                  <div className="hamster__ear"></div>
-                  <div className="hamster__eye"></div>
-                  <div className="hamster__nose"></div>
-                </div>
-                <div className="hamster__limb hamster__limb--fr"></div>
-                <div className="hamster__limb hamster__limb--fl"></div>
-                <div className="hamster__limb hamster__limb--br"></div>
-                <div className="hamster__limb hamster__limb--bl"></div>
-                <div className="hamster__tail"></div>
-              </div>
-            </div>
-            <div className="spoke"></div>
-          </div>
-        </div>
-      )}
+      {loading && <HamsterLoader />}
 
-      {!loading && (
-        <div style={{ display: (recipes.length > 0) ? "block" : "none" }}>
-          <p>Here are some recipe suggestions, click on any of them to get detailed instructions.</p>
-          <div className="container">
-            {(recipes.length > 0) ? showRecipes : null}
-          </div>
+      <div style={{ display: (recipes.length > 0) ? "block" : "none" }}>
+        <p>Here are some recipe suggestions, click on any of them to get detailed instructions.</p>
+        <div className="container">
+          {(recipes.length > 0) ? showRecipes : null}
         </div>
-      )}
+      </div>
 
       <div style={{ display: (instructions) ? "block" : "none" }}>
         <h2>Recipe Instructions</h2>
--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -1,15 +1,18 @@
 interface RecipeCardProps {
-    readonly recipeName: string
-    onPress(): void
+  readonly recipeName: string
+  readonly disabled: boolean
+  onClick: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 const RecipeCard = (props: RecipeCardProps) => {
-    return (
-        <div className="recipe-card">
-            <h3>{props.recipeName}</h3>
-            <button onClick={props.onPress}>How to make this recipe ?</button>
-        </div>
-    )
+  return (
+    <div className="recipe-card">
+      <h3>{props.recipeName}</h3>
+      <button onClick={props.onClick} disabled={props.disabled}>
+        How to make this recipe ?
+      </button>
+    </div>
+  )
 }
 
 export default RecipeCard
