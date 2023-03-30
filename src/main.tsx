765af7970c59e52f53799f67afc49e709a912a20
RyukShi <aure16300P@gmail.com>
2023-03-30 19:20:07 +0200

    disableReactDevTools added to main.js

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import LanguageProvider from './providers/LanguageProvider'
 import './index.css'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+if (import.meta.env.MODE === 'production') disableReactDevTools()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
